d5cb6da10e976947da674035e14f087ef9485841
deepanshurawat01 <73733617+deepanshurawat01@users.noreply.github.com>
2022-07-22 12:24:01 +0530

    Update script.js

--- form validation/script.js
+++ form validation/script.js
@@ -1,131 +1,45 @@
-$(document).ready(function(){
-    $('#usernamevalidation').hide();
-    $('#passwordvalidation').hide();
-    $('#confirmpasswordvalidation').hide();
-    
-    var err=true;
-    var password_error=true;
-    var confirm_password_error=true;
-    var confirm_email = true;
-    
-    $('#username').keyup(function(){
-        username_validation();
-    });
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1 class="text-center">Form Validation</h1>
+    <div class="container">
+       <div class="col-lg m-auto d-block">
+        <form action="#">
+            <div class="form-group">
+                <label for="user">Username:</label>
+                <input type="text" name="username" id="username" class="form-control" autocomplete="off">
+                <h5 id="usernamevalidation"></h5>
+            </div>
+            <div class="form-group">
+                <label for="email">Confirm Email:</label>
+                <input type="text" name="email" id="email" class="form-control" autocomplete="off">
+                <h5 id="emailvalidation"></h5>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="text" name="pass" id="password" class="form-control" autocomplete="off">
+                <h5 id="passwordvalidation"></h5>
+            </div>
+            <div class="form-group">
+                <label for="conpassword">Confirm Password:</label>
+                <input type="text" name="pass" id="confirmpassword" class="form-control" autocomplete="off">
+                <h5 id="confirmpasswordvalidation"></h5>
+            </div>
+            <input type="submit" value="submit" id="submitvalidation" class="btn btn-primary">
+        </form>
+       </div>
 
-    function username_validation(){
-        var username_val = $('#username').val();
-        if(username_val.length==''){
-            $('#usernamevalidation').show();
-            $('#usernamevalidation').html('Username Cannot be empty');
-            $('#usernamevalidation').css('color','red');
-            err=false;
-            return false;
-        }
-        else{
-
-            $('#usernamevalidation').hide();
-        }
-        if(username_val.length<4){
-            $('#usernamevalidation').show();
-            $('#usernamevalidation').html('Username Should have altleast 4 characters');
-            $('#usernamevalidation').css('color','red');
-            Error=false;
-            return false;
-        }
-        else{
-
-            $('#usernamevalidation').hide();
-        }
-    }
-    $('#password').keyup(function(){
-        password_validation();
-    });
-    
-    function password_validation(){
-        var password_val = $('#password').val();
-        if(password_val.length==''){
-            $('#passwordvalidation').show();
-            $('#passwordvalidation').html('Passsword cannot be empty');
-            $('#passwordvalidation').css('color','red');
-            password_error=false;
-            return false;
-        }
-        else{
-
-            $('#passwordvalidation').hide();
-        }
-        if(password_val.length<7){
-            $('#passwordvalidation').show();
-            $('#passwordvalidation').html('Please type password in correct format');
-            $('#passwordvalidation').css('color','red');
-            password_error=false;
-            return false;
-        }
-        else{
-
-            $('#passwordvalidation').hide();
-        }
-    }
-    $('#confirmpassword').keyup(function(){
-        confirm_password();
-    });
-    function confirm_password(){
-        var confirm_password_val = $('#confirmpassword').val();
-        var password_val = $('#password').val();
-        if(password_val!=confirm_password_val){
-            $('#confirmpasswordvalidation').show();
-            $('#confirmpasswordvalidation').html('Password did not match');
-            $('#confirmpasswordvalidation').css('color','red');
-            confirm_password_error=false;
-            return false;
-        }
-        else{
-            $('#confirmpasswordvalidation').hide();
-        }
-    }
-
-    $('#submitvalidation').click(function(){
-         username_validation();
-         password_validation();
-         confirm_password();
-
-         if(err==true && password_error==true && confirm_password_error==true){
-            return true;
-         }
-         else{
-            return false;
-         }
-
-    });
-    
-    $('#email').keyup(function(){
-        email_validation();
-    });
-
-    function email_validation(){
-        var email_val = $('#email').val();
-        if(email_val.length==''){
-            $('#emailvalidation').show();
-            $('#emailvalidation').html('Email cannot be empty');
-            $('#emailvalidation').css('color','red');
-            err=false;
-            return false;
-        }
-        else{
-
-            $('#emailvalidation').hide();
-        }
-        if(email_val.length<4){
-            $('#emailvalidation').show();
-            $('#emailvalidation').html('Email Should be valid');
-            $('#emailvalidation').css('color','red');
-            Error=false;
-            return false;
-        }
-        else{
-
-            $('#emailvalidation').hide();
-        }
-    }
-});
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</body>
+</html>
 
